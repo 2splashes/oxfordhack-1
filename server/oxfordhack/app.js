64d73e641bcb96f041384294822cebb4f4677f2b
James Tautges <jamesdtautges@gmail.com>
2016-11-19 17:54:27 +0000

    First crack at auth

--- server/oxfordhack/app.js
+++ server/oxfordhack/app.js
@@ -6,18 +6,39 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var handlebars = require('express-handlebars');
 
+var passport = require('passport');
+var Strategy = require('passport-facebook').Strategy;
+
 var index = require('./routes/index');
 var login = require('./routes/auth/login');
 var logout = require('./routes/auth/logout');
 
 var app = express();
 
+// We need to remove the actual keys from the source at some point
+passport.use(new Strategy({
+        clientID: '365354147137718',
+        clientSecret: '115a9b29823e452fa20dd7060f7cab80',
+        callbackURL: '/login/facebook/return'
+    },
+    function (accessToken, refreshToken, profile, cb) {
+        return cb(null, profile);
+    }));
+
+passport.serializeUser(function (user, cb) {
+    cb(null, user);
+});
+
+passport.deserializeUser(function (obj, cb) {
+    cb(null, obj);
+});
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 
 app.engine('handlebars', handlebars({
-  layoutsDir: path.join(__dirname,'views/layouts'),
-  partialsDir: path.join(__dirname, 'views/partials')
+    layoutsDir: path.join(__dirname, 'views/layouts'),
+    partialsDir: path.join(__dirname, 'views/partials')
 }));
 app.set('view engine', 'handlebars');
 
@@ -25,30 +46,38 @@ app.set('view engine', 'handlebars');
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(require('express-session')({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use('/', index);
 app.use('/login', login);
 app.use('/logout', logout);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+app.use(function (req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 });
 
 // error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.use(function (err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error.handlebars');
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error.handlebars');
 });
 
 module.exports = app;
--- server/oxfordhack/routes/auth/login.js
+++ server/oxfordhack/routes/auth/login.js
@@ -1,8 +1,17 @@
 var express = require('express');
 var router = express.Router();
+var passport = require('passport');
 
-router.get('/login', function(req, res, next) {
-    res.render('auth/login');
+router.get('/', function(req, res, next) {
+    res.render('layouts/auth/login');
+});
+
+router.get('/facebook', passport.authenticate('facebook'));
+
+router.get('/facebook/return',
+    passport.authenticate('facebook', {failureRedirect: '/login'}),
+    function (req, res) {
+        res.redirect('/');
 });
 
 module.exports = router;
