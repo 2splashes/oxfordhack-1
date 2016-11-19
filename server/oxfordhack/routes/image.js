780458e9b617a1ea1adc1984a094b3ab06295dd6
James Tautges <jamesdtautges@gmail.com>
2016-11-19 23:03:05 +0000

    Convert image count to int

--- server/oxfordhack/routes/image.js
+++ server/oxfordhack/routes/image.js
@@ -2,12 +2,13 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function (req, res, next) {
-	res.render('layouts/image', {count: 0});
+	res.render('layouts/image', {count: 1});
 });
 
 router.get('/:count', function (req, res, next){
-	if (req.params.count < 3) {
-		res.render('layouts/image', {count: req.params.count+1});
+	var count = parseInt(req.params.count);
+	if (count < 3) {
+		res.render('layouts/image', {count: count+1});
 	} else {
 		res.redirect('/results');
 	}
