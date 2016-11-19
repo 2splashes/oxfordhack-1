0a007706c787f40b181a39fbcb20ec0600b112b1
James Tautges <jamesdtautges@gmail.com>
2016-11-19 20:50:34 +0000

    Move auth check to avoid loop

--- server/oxfordhack/routes/index.js
+++ server/oxfordhack/routes/index.js
@@ -2,6 +2,13 @@ var express = require('express');
 var router = express.Router();
 var passport = require('passport');
 
+var isAuthenticated = function (req, res, next) {
+    if (req.isAuthenticated()) return next();
+    else res.redirect('/login');
+}
+
+router.use(isAuthenticated);
+
 /* GET home page. */
 router.get('/', function (req, res, next) {
     res.redirect('layout/index');
