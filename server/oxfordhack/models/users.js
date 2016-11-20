5ec26af292dd81dec0dc08cf5916277c5e194880
James Tautges <jamesdtautges@gmail.com>
2016-11-20 05:30:02 +0000

    Model user

---
+++ server/oxfordhack/models/users.js
@@ -0,0 +1,36 @@
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/data');
+
+var User = mongoose.model('User', { name: String,
+    id: {type: Number, index: {unique: true}},
+    history: [{
+    date: {type: Date, default: Date.now},
+    anger: Number,
+    contempt: Number,
+    disgust: Number,
+    fear: Number,
+    happiness: Number,
+    neutral: Number,
+    sadness: Number,
+    surprise: Number }] });
+
+var addUser = function (id) {
+    var n = new User({name: id, history: []});
+    n.save();
+};
+
+var addResult = function (id, result) {
+    User.findByIdAndUpdate(
+        id,
+        {$push: {history: result}},
+        {safe: true, upsert: true, new : true},
+        function(err, model) {
+            console.log(err);
+        }
+    );
+};
+
+module.exports = {
+    addUser: addUser,
+    addResult: addResult
+};
