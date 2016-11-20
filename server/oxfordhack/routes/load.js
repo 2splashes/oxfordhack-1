f1f1619152487e3cc7d73de6cdfa2e7891b1b9d9
James Tautges <jamesdtautges@gmail.com>
2016-11-20 04:55:15 +0000

    Hands

--- server/oxfordhack/routes/load.js
+++ server/oxfordhack/routes/load.js
@@ -3,21 +3,25 @@ var router = express.Router();
 var multer = require('multer');
 var upload = multer({dest: 'public/uploads'});
 var request = require('request');
+var fs = require('fs');
 
 var options = {
     url: 'https://api.projectoxford.ai/emotion/v1.0/recognize',
     headers: {
+        'Content-Type': 'application/json',
         'Ocp-Apim-Subscription-Key': "1521013cecd149fca7543bac79c8784d"
     }
 };
 
 router.post('/', upload.single('webcam'), function (req, res, next) {
-    options.form = {
-        url: 'https://ec2-54-191-173-129.us-west-2.compute.amazonaws.com/uploads/' + req.file.filename
-    };
-    console.log(options.form.url);
-    request(options, function (err, res, body) {
-        console.log(body);
+    fs.rename(req.file.path, req.file.path + '.jpeg', function () {
+        options.form = {
+            url: 'http://ec2-54-191-173-129.us-west-2.compute.amazonaws.com/uploads/' + req.file.filename + '.jpeg'
+        };
+        console.log(options.form.url);
+        request(options, function (err, res, body) {
+            console.log(body);
+        });
     });
 });
 
