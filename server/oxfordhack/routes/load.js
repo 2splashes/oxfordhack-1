833ebd422e389935f194269abdb14ee95089a7c3
James Tautges <jamesdtautges@gmail.com>
2016-11-20 05:02:14 +0000

    Trying something else?

--- server/oxfordhack/routes/load.js
+++ server/oxfordhack/routes/load.js
@@ -15,13 +15,13 @@ var options = {
 
 router.post('/', upload.single('webcam'), function (req, res, next) {
     fs.rename(req.file.path, req.file.path + '.jpeg', function () {
-        var body = {
+        options.json = {
             url: 'ec2-54-191-173-129.us-west-2.compute.amazonaws.com/uploads/' + req.file.filename + '.jpeg'
         };
-        console.log(body);
+        console.log(options.json);
         request(options, function (err, res, body) {
             console.log(body);
-        }).json(body);
+        });
     });
 });
 
