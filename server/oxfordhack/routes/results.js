26f0da6c74ce2887cdaa26de5dac38fceb0c2c31
James Tautges <jamesdtautges@gmail.com>
2016-11-20 07:30:31 +0000

    sdfoheigraih

--- server/oxfordhack/routes/results.js
+++ server/oxfordhack/routes/results.js
@@ -6,10 +6,8 @@ router.get('/', function (req, res, next) {
     Users.getHistory(req.user.id, function (history) {
         if (history[0].sadness > history[1].sadness) {
             res.render('layouts/result', {message: 'You seem a little down. Do you want to talk to someone about it?'});
-            next();
         } else {
             res.render('layouts/result', {message: 'Thanks for playing! Have a wonderful day.'});
-            next();
         }
     });
 });
