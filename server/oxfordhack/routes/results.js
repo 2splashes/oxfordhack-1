713f4d66f373be3e1a9906bb37b7f640e2eeda6b
James Tautges <jamesdtautges@gmail.com>
2016-11-20 07:21:30 +0000

    Fixes

--- server/oxfordhack/routes/results.js
+++ server/oxfordhack/routes/results.js
@@ -3,7 +3,7 @@ var router = express.Router();
 var Users = require('../models/users');
 
 router.get('/', function (req, res, next) {
-    Users.getHistory(profile.id, function (history) {
+    Users.getHistory(req.user.id, function (history) {
         if (history[0].sadness > history[1].sadness) {
             res.render('layouts/result', {message: 'You seem a little down. Do you want to talk to someone about it?'});
             next();
